0ae06a08cafdd240464b0e207f071f00ca374f74
zxc11086 <3031098162@qq.com>
2023-08-21 12:51:47 +0800

    kmp理解

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -1,11 +1,15 @@
 /*
     kmp算法，用于判断一个字符串是否是另一个字符串的子串
+    1.初始化j=-1，表示pattern当前已被匹配的最后位置
+    2.让i遍历文本串text，对每个i，执行3，4来是试图匹配text[i]和pattern[j+1]
+    3.不断令j=next[j],直到j回退到-1，或者text[i]==pattern[j+1]
+    4.如果text[i]==pattern[j+1],则j++，如果j==m-1，则说明pattern使text的字串
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> getNext(string s){
+vector<int> getNext(string s){  //求next数组
     int len = s.length();
     vector<int>next(len);
     int j=-1;
@@ -22,7 +26,7 @@ vector<int> getNext(string s){
     return next;
 }
 
-bool KMP(string text,string pattern){
+bool KMP(string text,string pattern){   //kmp算法
     int n=text.length();
     int m=pattern.length();
     vector<int>next=getNext(pattern); 
